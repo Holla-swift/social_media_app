8ae65d5fa7a629838936cf1c631bbb5112a2b4aa
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-14 23:59:30 +0100

    added html

--- core/views.py
+++ core/views.py
@@ -6,3 +6,18 @@ def index(request):
         
     }
     return render(request, 'index.html', context)
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST('username')
+        email = request.POST('email')
+        password = request.POST('password')
+        password = request.POST('password2')
+    else:
+        return render(request, 'signup.html', context)
+
+    context = {
+        
+    }
+    
