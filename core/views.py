619402f5892a031ccc69466ee7519a854ff03035
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-15 01:37:43 +0100

    stopped at signin, got work todo

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def index(request):
@@ -33,12 +34,22 @@ def signup(request):
                                                 )
                 user.save()
 
+                # return user and redirect to seeting page
+
+                # create porgile for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
 
         else:
              messages.info(request, 'Password Not Match')
              return redirect('signup')
-
-    
     
     else:
         return render(request, 'signup.html')
+    
+
+
+def signin(request):
+    return render(request, 'signin.html')
