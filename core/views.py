c2b012cb76940ead0ce46a51a7efe871e4499e1a
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-15 01:05:40 +0100

    added signup page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,14 +12,33 @@ def index(request):
 def signup(request):
 
     if request.method == "POST":
-        username = request.POST('username')
-        email = request.POST('email')
-        password = request.POST('password')
-        password = request.POST('password2')
-    else:
-        return render(request, 'signup.html', context)
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-    context = {
-        
-    }
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exist')
+                return redirect('signup')
+            
+            else:
+                user = User.objects.create_user(username=username,
+                                                email=email,
+                                                password=password
+                                                )
+                user.save()
+
+
+        else:
+             messages.info(request, 'Password Not Match')
+             return redirect('signup')
+
     
+    
+    else:
+        return render(request, 'signup.html')
